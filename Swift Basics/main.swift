46f4d47c5ab7c375ed3b21c40a44e769c5c8872d
Davis Haeusser <drhaeusser@gmail.com>
2019-09-01 19:05:43 -0500

    Initial Commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Davis Haeusser on 9/1/19.
+//  Copyright © 2019 Davis Haeusser. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
